c2fe434f0cb2c2c2d29949841832bf813c2b801d
Marcel von Wyl <mrvonwyl@gmail.com>
2020-01-15 17:00:58 +0100

    hello world usage in app component

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelloWorldService } from '@mrvonwyl/hello';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private helloWorldService: HelloWorldService) {}
+
+  ngOnInit() {
+    this.title = this.helloWorldService.hello();
+  }
 }
--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloModule } from '@mrvonwyl/hello';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HelloModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
